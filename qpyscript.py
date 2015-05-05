137c9133d55ec5b6edd66545915d97a5cbf0953c
Peleg Michaeli <freepeleg@gmail.com>
2015-05-05 09:37:14 +0300

    Using pyslext

--- qpyscript.py
+++ qpyscript.py
@@ -7,8 +7,9 @@ General framework for quick scripts.
 ## System
 import sys
 
-## Shell
+## Argument parsing
 import argparse as ap
+from pyslext.argparsing import HelpFormatter, StoreDatetime
 
 
 ## Console
@@ -26,25 +27,6 @@ def xcolor(clr):
 def nocolor():
     """ Reset / normal. """
     return csi(0, 'm')
-
-
-## Argument parsing
-class HelpFormatter(ap.HelpFormatter):
-  """ A simple Help Formatter which obeys the 79 chars convention. """
-  def __init__(self, *args, **kwargs):
-    kwargs.update(width=79)
-    super(HelpFormatter, self).__init__(*args, **kwargs)
-
-
-class StoreDatetime(ap.Action):
-  def __init__(self, option_strings, dest, default=None, required=False,
-               help=None, metavar=None):
-    super(StoreDatetime, self).__init__(
-        option_strings=option_strings, dest=dest, nargs=None, default=default,
-        required=required, help=help, metavar=metavar)
-
-  def __call__(self, parser, namespace, values, option_string=None):
-    setattr(namespace, self.dest, dp.parse(values))
 
 
 class Argument(object):
